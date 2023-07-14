d5e98a71be1585751eb507055b0a140bd9b6a048
Sacettin Şahin <sacettin.sahin@etiya.com>
2023-07-14 08:45:25 +0300

    Post form created
    Post form values was editted

--- src/app/admin/pages/new-post/new-post.component.ts
+++ src/app/admin/pages/new-post/new-post.component.ts
@@ -1,53 +1,80 @@
 import { Component, OnInit } from '@angular/core';
 import { CategoritesService } from '../../services/categorites.service';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Post } from '../../models/post';
 
 @Component({
   selector: 'app-new-post',
   templateUrl: './new-post.component.html',
-  styleUrls: ['./new-post.component.scss']
+  styleUrls: ['./new-post.component.scss'],
 })
 export class NewPostComponent implements OnInit {
+  permalink: string = '';
+  imgSrc: any = 'https://placehold.co/100x100';
+  selectedImg: any;
+  categories: any[];
 
-  permalink:string=""
-  imgSrc:any="https://placehold.co/100x100"
-  selectedImg:any
-  categories:any[]
-  
-  postForm:FormGroup
+  postForm: FormGroup;
 
-  constructor(private categoryService: CategoritesService, private fb:FormBuilder) { 
+  constructor(
+    private categoryService: CategoritesService,
+    private fb: FormBuilder
+  ) {
     this.postForm = this.fb.group({
-      title: ["" ,[Validators.required, Validators.minLength(10)]],
-      permalink: ["",Validators.required],
-      excerpt: ["",[Validators.required, Validators.minLength(50)]],
-      category: ["",Validators.required],
-      postImg: ["",Validators.required],
-      content: ["",Validators.required],
-    })
+      title: ['', [Validators.required, Validators.minLength(10)]],
+      permalink: ['', Validators.required],
+      excerpt: ['', [Validators.required, Validators.minLength(50)]],
+      category: ['', Validators.required],
+      postImg: ['', Validators.required],
+      content: ['', Validators.required],
+    });
   }
 
-  get fc(){
-    return this.postForm.controls
+  get fc() {
+    return this.postForm.controls;
   }
 
   ngOnInit(): void {
-    this.categoryService.loadData().subscribe(res=>{
-      this.categories = res
-    })
+    this.categoryService.loadData().subscribe((res) => {
+      this.categories = res;
+    });
   }
 
-  onTitleChanged($event){
-    const title = $event.target.value
-    this.permalink= title.replace(/\s/g,"-")
+  onTitleChanged($event) {
+    const title = $event.target.value;
+    this.permalink = title.replace(/\s/g, '-');
   }
-  showPreview($event){
-    const reader = new FileReader()
+  showPreview($event) {
+    const reader = new FileReader();
     reader.onload = (e) => {
-      this.imgSrc = e.target.result
-    }
+      this.imgSrc = e.target.result;
+    };
 
-    reader.readAsDataURL($event.target.files[0])
+    reader.readAsDataURL($event.target.files[0]);
     this.selectedImg = $event.target.files[0];
+  }
+  onSubmit() {
+    console.log(this.postForm.value);
+
+    let splittedCategoryArray = this.postForm.value.category.split("-");
+
+
+    const postData: Post = {
+      title: this.postForm.value.title,
+      permalink: this.postForm.value.permalink,
+      category: {
+        categoryId: splittedCategoryArray[0],
+        category: splittedCategoryArray[1],
+      },
+      postImgPath: "",
+      excerpt: this.postForm.value.excerpt,
+      content: this.postForm.value.content,
+      isFeatured: false,
+      views: 0,
+      status: "new",
+      createdAt: new Date(),
+    };
+
+    console.log("post data: ",postData)
   }
 }
